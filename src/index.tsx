94a0587893a06a2622de27b4d2a2d94d71e3dd5d
Amit Sharma <amit12.sharma@gmail.com>
2026-06-20 21:21:34 +0200

    fix: code cleanup

--- src/index.tsx
+++ src/index.tsx
@@ -2,53 +2,20 @@ import React from 'react'
 
 import WorldMap from 'react-svg-worldmap'
 
-import type { CountryContext, Data, SizeOption } from 'react-svg-worldmap'
+import type { CountryContext, SizeOption } from 'react-svg-worldmap'
 import { defaultData } from './rawData/getDefaultMapData'
-
-const populationData: Data<string> = [
-  { country: 'cn', value: JSON.stringify({ iso: 'CN' }) }, // China
-  { country: 'in', value: JSON.stringify({ iso: 'IN' }) }, // India
-  { country: 'us', value: JSON.stringify({ iso: 'US' }) }, // United States
-  { country: 'id', value: JSON.stringify({ iso: 'ID' }) }, // Indonesia
-  { country: 'br', value: JSON.stringify({ iso: 'BR' }) }, // Brazil
-  { country: 'ng', value: JSON.stringify({ iso: 'NG' }) }, // Nigeria
-  { country: 'ru', value: JSON.stringify({ iso: 'RU' }) }, // Russia
-  { country: 'mx', value: JSON.stringify({ iso: 'MX' }) }, // Mexico
-  { country: 'au', value: JSON.stringify({ iso: 'AU' }) }, // Australia
-]
 
 const getStyle = () => {
   return {
-    fill: 'red',
-    fillOpacity: 0,
-    stroke: 'green',
-    strokeWidth: 1,
-    strokeOpacity: 0.2,
+    fill: '#ffffff',
+    fillOpacity: 2,
+    stroke: '#000000',
+    strokeWidth: 1.2,
+    strokeOpacity: 0.7,
     cursor: 'pointer',
     outline: 'none',
   }
 }
-
-// const createTextLabels = (width: number) => {
-//   const labels: ({ label: string } & ComponentProps<'text'>)[] = [
-//     { label: 'Atlantic Ocean', x: 0.37 * width, y: 0.39 * width },
-//     { label: 'Indian Ocean', x: 0.69 * width, y: 0.57 * width },
-//     { label: 'Pacific Ocean', x: 0.083 * width, y: 0.48 * width },
-//     {
-//       label: 'Arctic Ocean',
-//       x: 0.75 * width,
-//       y: 0.058 * width,
-//       style: { fill: 'blue' },
-//     },
-//   ]
-//   if (width < 550) {
-//     return labels.map((label) => ({
-//       ...label,
-//       style: { ...label.style, fontSize: '30%' },
-//     }))
-//   }
-//   return labels
-// }
 
 export interface ExtendedWorldMapProps {
   title?: string
@@ -70,16 +37,16 @@ export const ExtendedWorldMap = ({ title, size, onClick, infoLink }: ExtendedWor
       onClickFunction={(countryContext: CountryContext<string>) => {
         onClick && onClick(countryContext)
       }}
-      // tooltipTextFunction={(countryContext: CountryContext<string>) => {
-      //   const populationInfo = JSON.parse(countryContext.countryValue || '{}')
-      //   return `${countryContext.countryName} - ${populationInfo.iso}`
-      // }}
+      tooltipTextFunction={(countryContext: CountryContext<string>) => {
+        return `${countryContext.countryName}`
+      }}
       // hrefFunction={(countryContext: CountryContext<string>) => {
       //   return infoLink
       //     ? infoLink(countryContext)
       //     : `https://en.wikipedia.org/wiki/${countryContext.countryName}`
       // }}
       styleFunction={getStyle}
+      // regionClassName="no-hover"
       // textLabelFunction={undefined}
     />
   )
